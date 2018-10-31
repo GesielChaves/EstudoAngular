97020c715e71a13675449979fe4d210868824052
gesiel <gesiel@DESKTOP-G0VIC3B>
2018-10-30 21:06:40 -0300

    Finalizando login

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
 @NgModule({
     declarations: [SignInComponent],
-    imports: [CommonModule, ReactiveFormsModule, VMessageModule]
+    imports: [CommonModule, ReactiveFormsModule, VMessageModule, RouterModule]
 })
 export class HomeModule { }
