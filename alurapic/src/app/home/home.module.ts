ab2b642a53f59c3e0ff09c32daff52d173017a69
gesiel <gesiel@DESKTOP-G0VIC3B>
2018-11-01 13:34:56 -0300

    Formmulario para criar usuário

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './signup/signup.component';
 @NgModule({
-    declarations: [SignInComponent],
-    imports: [CommonModule, ReactiveFormsModule, VMessageModule, RouterModule]
+    declarations: [SignInComponent, SignUpComponent],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, VMessageModule, RouterModule]
 })
 export class HomeModule { }
