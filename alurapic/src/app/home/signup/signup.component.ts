6263beb0bc0e520fb211d6df6fa1ee5c62d86130
gesiel <gesiel@DESKTOP-G0VIC3B>
2018-11-03 23:30:06 -0300

    Rotas Filhas

--- alurapic/src/app/home/signup/signup.component.ts
+++ alurapic/src/app/home/signup/signup.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 import { NewUser } from './new-user';
 import { SignUpService } from './signup.service';
 import { Router } from '@angular/router';
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
 
 @Component({
     templateUrl: './signup.component.html'
 })
 export class SignUpComponent implements OnInit {
     signupForm: FormGroup;
-
+    @ViewChild('inputEmail') inputEmail: ElementRef<HTMLInputElement>;
     constructor(private formBuilder: FormBuilder,
         private userNotTakenValidatorService: UserNotTakenValidatorService,
         private signUpService: SignUpService,
-        private router: Router) { }
+        private router: Router,
+        private platformDetectorService: PlatformDetectorService) { }
 
     ngOnInit(): void {
         this.signupForm = this.formBuilder.group({
@@ -29,6 +31,8 @@ export class SignUpComponent implements OnInit {
                 this.userNotTakenValidatorService.checkUserNameTaken()],
             password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]]
         });
+
+        this.platformDetectorService.isPlatformBrowser() && this.inputEmail.nativeElement.focus();
     }
 
     signUp() {
