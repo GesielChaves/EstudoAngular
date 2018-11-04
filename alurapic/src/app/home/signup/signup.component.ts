610d21484d9956053d8e44917b19ba19dcef77a3
gesiel <gesiel@DESKTOP-G0VIC3B>
2018-11-03 21:57:52 -0300

    Criando usuario

--- alurapic/src/app/home/signup/signup.component.ts
+++ alurapic/src/app/home/signup/signup.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+import { NewUser } from './new-user';
+import { SignUpService } from './signup.service';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './signup.component.html'
@@ -10,7 +13,9 @@ export class SignUpComponent implements OnInit {
     signupForm: FormGroup;
 
     constructor(private formBuilder: FormBuilder,
-        private userNotTakenValidatorService: UserNotTakenValidatorService) { }
+        private userNotTakenValidatorService: UserNotTakenValidatorService,
+        private signUpService: SignUpService,
+        private router: Router) { }
 
     ngOnInit(): void {
         this.signupForm = this.formBuilder.group({
@@ -24,5 +29,16 @@ export class SignUpComponent implements OnInit {
                 this.userNotTakenValidatorService.checkUserNameTaken()],
             password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]]
         });
+    }
+
+    signUp() {
+        const newUser = this.signupForm.getRawValue() as NewUser;
+
+        this.signUpService
+            .sigUp(newUser)
+            .subscribe(() =>
+                this.router.navigate(['']),
+                err => console.log(err)
+            );
     }
 }
