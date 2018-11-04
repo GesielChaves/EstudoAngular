fe20753430148ad60907338511192763c40c254c
gesiel <gesiel@DESKTOP-G0VIC3B>
2018-11-04 00:09:14 -0300

    Especificando o use dos services de home

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -6,8 +6,10 @@ import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
 import { SignUpComponent } from './signup/signup.component';
 import { HomeComponent } from './home.component';
+import { SignUpService } from './signup/signup.service';
 @NgModule({
     declarations: [SignInComponent, SignUpComponent, HomeComponent],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, VMessageModule, RouterModule]
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, VMessageModule, RouterModule],
+    providers: [SignUpService]
 })
 export class HomeModule { }
