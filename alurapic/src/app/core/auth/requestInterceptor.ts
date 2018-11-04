9795d692e14f0a2721d7ff56d5b63f9276bcb209
gesiel <gesiel@DESKTOP-G0VIC3B>
2018-11-04 00:24:07 -0300

    Interceptor e envio de token

---
+++ alurapic/src/app/core/auth/requestInterceptor.ts
@@ -0,0 +1,26 @@
+// tslint:disable-next-line:max-line-length
+import { HttpRequest, HttpHandler, HttpSentEvent, HttpHeaderResponse, HttpUserEvent, HttpProgressEvent, HttpInterceptor, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TokenService } from '../token/token.service';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class RequestInterceptor implements HttpInterceptor {
+
+    constructor(private tokenService: TokenService) { }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpSentEvent | HttpHeaderResponse
+        | HttpProgressEvent | HttpResponse<any> | HttpUserEvent<any>> {
+
+        if (this.tokenService.hasToken()) {
+            const token = this.tokenService.getToken();
+            req = req.clone({
+                setHeaders: {
+                    'x-access-token': token
+                }
+            });
+        }
+        return next.handle(req);
+    }
+
+}
